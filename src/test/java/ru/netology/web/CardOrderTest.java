b9538d82907be453d220d3d57a2c381005bb4d19
Neto test <strokova-92@mail.ru>
2023-07-23 18:07:17 +0300

    fixed test

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,16 +1,14 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,20 +16,23 @@ class CallbackTest<list> {
     private WebDriver driver;
 
     @BeforeAll // хук, который запускает драйвер
-    static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeEach // Выполняет действия до выполнения теста
-    void setUp(){
+    @BeforeEach
+        // Выполняет действия до выполнения теста
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);}
+        driver = new ChromeDriver(options);
+    }
 
-    @AfterEach // выполняет действие после завершения теста
-    void tearDown(){
+    @AfterEach
+        // выполняет действие после завершения теста
+    void tearDown() {
         driver.quit(); // заканчивает работу
         driver = null; // обнуляет его
     }
@@ -41,18 +42,13 @@ class CallbackTest<list> {
         // загрузить страницу
         driver.get("http://localhost:9999/");
 
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванов Иван");
-        elements.get(1).sendKeys("+79990000000");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов-Петров Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79990000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
 
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());//trim удаляет лишние пробелы
-
-        Thread.sleep(5000);
-        // поиск элементов
-        //взаимодействие с элементами
-
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals(expected,actual);
     }
 }
